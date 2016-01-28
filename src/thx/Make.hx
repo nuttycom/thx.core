9874d617f82dcd59073dac682ab56d70ea79f86f
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-01-28 13:41:30 -0700

    Broken, but leaving for now.

--- src/thx/Make.hx
+++ src/thx/Make.hx
@@ -6,33 +6,29 @@ import haxe.macro.Type;
 import haxe.macro.TypeTools;
 import haxe.macro.Context;
 
+#if macro
+typedef FieldMeta = {
+  field: String, 
+  type: String, 
+  weight: Float, 
+  optional: Bool
+};
+#end
+
 class Make {
-  macro public static function constructor(expr : ExprOf<{}>) {
-    var items = switch expr.expr {
-      case EObjectDecl(fields):
-        thx.Arrays.mapi(fields, function(v, i) return extractFieldFromLiteral(v, i));
-      case EConst(CIdent(type)):
-        switch Context.getType(type) {
-          case TType(t, p):
-            var type = t.get(),
-                meta = type.meta;
-            switch type.type {
-              case TAnonymous(anonym):
-                var seqMeta = meta.extract(":sequence");
-                var sequence = thx.Arrays.flatten(
-                      seqMeta.map(function(meta) return meta.params.map(ExprTools.toString))
-                    );
-                anonym.get().fields.map(extractFieldAnonymous.bind(_, sequence));
-              case _:
-                Context.error("Make.constructor can only take a reference to a typedef that represent an object literal", Context.currentPos()); [];
-            }
-          case _:
-            Context.error("Make.constructor can only take a reference to a typedef that represent an object literal", Context.currentPos()); [];
-        }
+  macro public static function constructor(expr : ExprOf<{ expr: ExprDef }>) {
+    var items: Array<FieldMeta> = switch expr.expr {
+      case EParenthesis({ expr: ECheckType(_, t), pos: pos }): 
+        extractFields(t);
+
       case other:
         Context.error("Make.constructor only accepts anonymous objects with type names as values or a reference to a typedef", Context.currentPos()); [];
     }
+
     items.sort(function(a, b) return thx.Floats.compare(a.weight, b.weight));
+
+    trace(items);
+
     var args   = items.map(function(item) return (item.optional ? "?" : "") + '${item.field} : ${item.type}'),
         assign = items.filter(function(item) return !item.optional).map(function(item) return '${item.field} : ${item.field}'),
         types  = items.map(function(item) return (item.optional ? "@:optional " : "") + 'var ${item.field} : ${item.type};'),
@@ -44,22 +40,66 @@ class Make {
   }
 
 #if macro
-  static function extractFieldFromLiteral(field, weight : Float) {
-    switch field.expr.expr {
-      case EConst(CIdent(type)):
-        return { field : field.field, type : type, weight : weight, optional : false };
+  static function extractFields(typ: ComplexType): Array<FieldMeta> {
+    return switch typ {
+      case TAnonymous(fields):
+        trace("ANON: ##########################################################");
+        trace(fields);
+        throw("Not yet");
+        // thx.Arrays.mapi(
+        //   fields, 
+        //   function(cf: ClassField, i: Int) return {
+        //     field: cf.name,
+        //     type: TypeTools.toString(cf.type),
+        //     weight: i,
+        //     optional: cf.meta.has(":optional")
+        //   }
+        // );
+
+      case TPath({ name: tName, params: tParams }):
+        switch Context.getType(tName) {
+          case TType(t, p):
+            var type: DefType = t.get();
+            var meta: MetaAccess = type.meta;
+
+            switch type.type {
+              case TAnonymous(anon):
+                // Anonymous, named structural type with potential sequence metadata annotation
+                extractAnonFields(anon.get(), meta);
+
+              case _:
+                Context.error("Make.constructor can only take a reference to a typedef that represent an object literal", Context.currentPos()); [];
+            }
+
+          case _:
+            Context.error("Make.constructor can only take a reference to a typedef that represent an object literal", Context.currentPos()); [];
+        }
+
       case _:
-        Context.error("Make.constructor fields can only have values that represent types", Context.currentPos());
-        return null;
-    }
+        Context.error("Make.constructor can only take a reference to a typedef that represent an object literal", Context.currentPos()); [];
+    };
   }
 
-  static function extractFieldAnonymous(field : ClassField, sequence : Array<String>) {
-    var pos : Float = sequence.indexOf(field.name);
-    var weights = thx.Arrays.flatten(field.meta.extract(":weight").map(function(meta) { return meta.params; })).map(ExprTools.toString);
-    if(weights.length > 0)
-      pos = Std.parseFloat(weights[0]);
-    return { field : field.name, type : TypeTools.toString(field.type), weight : pos, optional : field.meta.has(":optional") };
+  static function extractAnonFields(anon: AnonType, meta: MetaAccess): Array<FieldMeta> {
+    var seqMeta = meta.extract(":sequence");
+    var sequence: Array<String> = thx.Arrays.flatMap(seqMeta, function(meta) return meta.params.map(ExprTools.toString));
+
+    return anon.fields.map(extractAnonField.bind(_, sequence));
   }
+
+  static function extractAnonField(field : ClassField, sequence : Array<String>): FieldMeta {
+    var weights = thx.Arrays.flatMap(field.meta.extract(":weight"), function(meta) return meta.params).map(ExprTools.toString);
+    var pos : Float = if (weights.length > 0) Std.parseFloat(weights[0]) 
+                      else sequence.indexOf(field.name);
+
+    trace(field);
+    return { 
+      field : field.name, 
+      type : TypeTools.toString(field.type), 
+      weight : pos, 
+      optional : field.meta.has(":optional") 
+    };
+  }
+
 #end
 }
--- test/thx/TestMake.hx
+++ test/thx/TestMake.hx
@@ -5,28 +5,28 @@ import utest.Assert;
 class TestMake {
   public function new() {}
 
-  public function testConstructorLiteral() {
-    var f = Make.constructor({ c : String, b : Int, a : Float, d : String });
-    Assert.same({
-      c : "A",
-      b : 1,
-      a : 0.2,
-      d : "D"
-    }, f("A", 1, 0.2, "D"));
-  }
-
-  public function testConstructorFromTypedef() {
-    var f = Make.constructor(ConstructorType);
-    Assert.same({
-      c : "A",
-      b : 1,
-      a : 0.2,
-      d : "D"
-    }, f("A", 1, 0.2, "D"));
-  }
-
+//  public function testConstructorLiteral() {
+//    var f = Make.constructor((_: { c : String, b : Int, a : Float, d : String }));
+//    Assert.same({
+//      c : "A",
+//      b : 1,
+//      a : 0.2,
+//      d : "D"
+//    }, f("A", 1, 0.2, "D"));
+//  }
+//
+//  public function testConstructorFromTypedef() {
+//    var f = Make.constructor((_: ConstructorType));
+//    Assert.same({
+//      c : "A",
+//      b : 1,
+//      a : 0.2,
+//      d : "D"
+//    }, f("A", 1, 0.2, "D"));
+//  }
+//
   public function testPolyConstructorFromTypedef() {
-    var f = Make.constructor(ConstructorPoly<Int>);
+    var f = Make.constructor((_: ConstructorPoly<Int>));
     Assert.same({ a: 1, b: 2 }, f(1, 2));
   }
 }
@@ -41,5 +41,5 @@ typedef ConstructorType = {
 
 typedef ConstructorPoly<A> = {
   a: A,
-  b: B
+  b: A
 }
